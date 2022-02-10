a3e1a612d86bf490e8da1e921ec9948b9d490ae7
pepijngriffioen <pepijngriffioen@hotmail.com>
2022-02-10 10:19:40 +0100

    code formatting updates

--- Flask-docker-example/coreference.py
+++ Flask-docker-example/coreference.py
@@ -28,7 +28,7 @@ class Coreference:
       self.pronouns = []
       predicted_ant = self.output['predicted_antecedents']
 
-      for index in range(len(predicted_ant)):
+      for index, item in enumerate(predicted_ant):
          if predicted_ant[index] != -1:
             ant_span = self.output['top_spans'][predicted_ant[index]]
             curr_span = self.output['top_spans'][index]
@@ -102,30 +102,29 @@ class Coreference:
 
    def find_all_personal_ant(self):
       """Finds all personal antecedents in the text."""
-      pps = self.return_personal_pronouns()
+      personal_pronouns = self.return_personal_pronouns()
       result = []
-      for pp in pps:
-         out = self.find_main_pronoun_antecedent(pp)
+      for personal_pronoun in personal_pronouns:
+         out = self.find_main_pronoun_antecedent(personal_pronoun)
          if out != "":
-            result.append([pp,out])
+            result.append([personal_pronoun,out])
       return result
-   
-   def replace_partial_text_with_coref(self,avo_sen:list,result_per_avo_sent:dict,avo_results_index:int,input_text:list) -> list:
+
+   def replace_partial_text_with_coref(self,avo_sen:list,result_per_avo_sent:dict,
+                                       avo_results_index:int,input_text:list) -> list:
       """Replace a piece of text with coref references.
       
          Assumption
-         - The given text where parts of the sentence are taken out have been substituted with three #'s
+         The given text where parts of the sentence are taken out have been
+            substituted with three #'s
       """
-      # select which avo_index we are talking about.
-      print("avo_results_index:{}".format(avo_results_index))
-      # avo_sen = avo_sents[avo_results_index]
       length_difference = 0
       action_copy = input_text[:]
       if avo_results_index in result_per_avo_sent.keys():
          coref_result = result_per_avo_sent[avo_results_index]
          sen_begin_index = avo_sen['begin_index']
          replacements = [[ res[0][0], res[1][0],
-                           res[2][1]-sen_begin_index, res[2][2]-sen_begin_index] 
+                           res[2][1]-sen_begin_index, res[2][2]-sen_begin_index]
                            for res in coref_result]
          for replacement in replacements:
             begin = replacement[2] + length_difference
@@ -138,16 +137,14 @@ class Coreference:
                length_difference += (len(new_word) - len(old_word))
             else:
                # in some cases we moved the word to the swimminglane.
-               # print("reference not found in action_text, so we don't replace")
                pass
       else:
          pass
       return action_copy
-   
+
    def order_coref_on_avo_index(self,avo_sents:list,coref_results:list,coref_text:list) -> dict:
       """Order the coreference result per avo_sentence index and add begin and end index."""
       result_per_avo_index = {}
-      # ppl_coref = Pipeline()
       com_method = cm.CommonFunctions()
       sentence_lengths = com_method.get_list_sent_lengths(" ".join(coref_text))
       avo_sents_ordered = com_method.order_avo_on_sent_index(avo_sents)
@@ -158,18 +155,21 @@ class Coreference:
          end_reference_sent_index = reference_result[0][1][1] - sent_begin_index
          possible_avos = avo_sents_ordered[sent_index]
          possible_ranges = [range(avo['begin_index'],avo['end_index']+1) for avo in possible_avos]
-         found_avo_indices = [index for index, test_range in enumerate(possible_ranges) if begin_reference_sent_index in test_range]
+         found_avo_indices = [index for index, test_range in enumerate(possible_ranges)
+                              if begin_reference_sent_index in test_range]
          avo_sent_indices = [avo_sents.index(possible_avos[avo_id]) for avo_id in found_avo_indices]
          for avo_sent_index in avo_sent_indices:
             reference_result_item = reference_result[:]
-            reference_result_item[2] = [reference_result_item[2],begin_reference_sent_index,end_reference_sent_index]
+            reference_result_item[2] = [reference_result_item[2],begin_reference_sent_index,
+                                       end_reference_sent_index]
             if avo_sent_index in result_per_avo_index.keys():
                result_per_avo_index[avo_sent_index].append(reference_result_item)
             else:
                result_per_avo_index[avo_sent_index] = [reference_result_item]
       return result_per_avo_index
 
-   def fill_swimming_lanes_and_coref_sents(self,avo_sents:list,coref_results:list,coref_text:list) -> list:
+   def fill_swimming_lanes_and_coref_sents(self,avo_sents:list,coref_results:list,
+                                             coref_text:list) -> list:
       """Fill swimming lanes and add coreference to sentences"""
       result_per_avo_sent = self.order_coref_on_avo_index(avo_sents,coref_results,coref_text)
       for avo_index, avo_sent in enumerate(avo_sents):
@@ -177,31 +177,40 @@ class Coreference:
          if avo_sent['sw_lane']:
             begin_sw = avo_sent['sw_lane'][0]
             end_sw = avo_sent['sw_lane'][1] + 1
-            #change all words to be a ### if they are not the swimminglane. Such that only the words we need are used for coreferencing.
-            rest_of_sent = [item if index not in range(begin_sw,end_sw) else "###" 
+            #change all words to be a ### if they are not the swimminglane.
+            # Such that only the words we need are used for coreferencing.
+            rest_of_sent = [item if index not in range(begin_sw,end_sw) else "###"
                               for index,item in enumerate(action_text)]
             coref_sent_result = self.replace_partial_text_with_coref(avo_sent,result_per_avo_sent,
                                  avo_index,rest_of_sent)
             # Keep the words that are not a ###
             sentence = [word for word in coref_sent_result if word != "###"]
-
-            coref_complete_sent = self.replace_partial_text_with_coref(avo_sent,result_per_avo_sent,avo_index,action_text)
             # replace all words other than the swimminglane with "###"
-            swimming_lane_in_sent = [item if index in range(begin_sw,end_sw) else "###" for index,item in enumerate(action_text)]
-            coref_swimming_lane_result = self.replace_partial_text_with_coref(avo_sent,result_per_avo_sent,avo_index,swimming_lane_in_sent)
+            swimming_lane_in_sent = [item if index in range(begin_sw,end_sw) else "###"
+                                       for index,item in enumerate(action_text)]
+            coref_swimming_lane_result = self.replace_partial_text_with_coref(avo_sent,
+                                                result_per_avo_sent,avo_index,swimming_lane_in_sent)
             swimming_lane = [word for word in coref_swimming_lane_result if word != "###"]
+            #complete sent
+            coref_complete_sent = self.replace_partial_text_with_coref(avo_sent,result_per_avo_sent,
+                                                                        avo_index,action_text)
             # assign the data back to the avo_sent result
             avo_sent['node_text'] = sentence
-            avo_sent['complete_sent'] = coref_complete_sent
             avo_sent['sw_lane_text'] = swimming_lane
+            avo_sent['complete_sent'] = coref_complete_sent
          else:
-            coref_sent_result = self.replace_partial_text_with_coref(avo_sent,result_per_avo_sent,avo_index,action_text)
+            coref_sent_result = self.replace_partial_text_with_coref(avo_sent,result_per_avo_sent,
+                                                                     avo_index,action_text)
             avo_sent['node_text'] = coref_sent_result
             avo_sent['complete_sent'] = coref_sent_result
 
 #demo
 # coref = Coreference()
-# inputDocument = 'The legal pressures facing Michael Cohen are growing in a wide-ranging investigation of his personal business affairs and his work on behalf of his former client, President Trump. In addition to his work for Mr. Trump, he pursued his own business interests, including ventures in real estate, personal loans and investments in taxi medallions.'
-# coref.connect(document=inputDocument)
+# INPUT_DOCUMENT = ('The legal pressures facing Michael Cohen are growing in a wide-ranging '
+#                   'investigation of his personal business affairs and his work on behalf '
+#                   'of his former client, President Trump. In addition to his work for Mr. '
+#                   'Trump, he pursued his own business interests, including ventures in '
+#                   'real estate, personal loans and investments in taxi medallions.')
+# coref.connect(document=INPUT_DOCUMENT)
 # coref.parse_data()
 # output = coref.find_all_personal_ant()
