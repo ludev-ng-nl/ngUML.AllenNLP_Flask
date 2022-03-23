beacb3a7915768c265ddac0d422c741ac1ed44a0
pepijngriffioen <pepijngriffioen@hotmail.com>
2022-03-23 22:54:25 +0100

    Added extra way of specifying an empty conditional

--- Flask-docker-example/indicators.py
+++ Flask-docker-example/indicators.py
@@ -1,13 +1,16 @@
 friedrich_conditional_indicators = [
-    'if',
-    'whether',
-    'in case of',
-    'in the case of',
-    'in case',
-    'for the case',
-    'whereas',
-    'otherwise',
-    'optionally'
+    "if",
+    "whether",
+    "in case of",
+    "in the case of",
+    "in case",
+    "for the case",
+    "whereas",
+    "optionally",
+]
+
+empty_conditional_indicators = [
+    "otherwise",
 ]
 
 ferreira_conditional_indicators = [
@@ -19,52 +22,59 @@ ferreira_conditional_indicators = [
     # 'until',
     # 'unless',
     # 'when'
-    # 'only if' 
+    # 'only if'
 ]
 
-self_conditional_indicators = [
-
-]
+self_conditional_indicators = []
 
 friedrich_parallel_indicators = [
-    'while',
-    'meanwhile',
-    'in parallel',
-    'concurrently',
-    'meantime', 
+    "while",
+    "meanwhile",
+    "in parallel",
+    "concurrently",
+    "meantime",
     # 'in the meantime'
 ]
 
 ferreira_parallel_indicators = [
-    # 'in parallel with this', 
-    # 'in addition to', 
-    'simultaneously', 
-    # 'at the same time', 
+    # 'in parallel with this',
+    # 'in addition to',
+    "simultaneously",
+    # 'at the same time',
     # 'whereas'
 ]
 
-self_parallel_indicators = [
-
-]
+self_parallel_indicators = []
 
 friedrich_sequence_indicators = [
-    'then',
-    'after',
-    'afterward', 
-    'afterwards', 
-    'subsequently', 
-    # 'based on this', 
-    'thus'
+    "then",
+    "after",
+    "afterward",
+    "afterwards",
+    "subsequently",
+    # 'based on this',
+    "thus",
 ]
 
 ferreira_sequence_indicators = [
     # todo - look at teh others
 ]
 
-self_sequence_indicators = [
+self_sequence_indicators = []
 
-]
-
-conditional_indicators = friedrich_conditional_indicators + ferreira_conditional_indicators + self_conditional_indicators
-parallel_indicators = friedrich_parallel_indicators + ferreira_parallel_indicators + self_parallel_indicators
-sequence_indicators = friedrich_sequence_indicators + ferreira_sequence_indicators + self_sequence_indicators
+conditional_indicators = (
+    friedrich_conditional_indicators
+    + ferreira_conditional_indicators
+    + self_conditional_indicators
+    + empty_conditional_indicators
+)
+parallel_indicators = (
+    friedrich_parallel_indicators
+    + ferreira_parallel_indicators
+    + self_parallel_indicators
+)
+sequence_indicators = (
+    friedrich_sequence_indicators
+    + ferreira_sequence_indicators
+    + self_sequence_indicators
+)
