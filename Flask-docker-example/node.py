884f0cb8ad3947858499e31ec496a936347e4599
pepijngriffioen <pepijngriffioen@hotmail.com>
2022-04-11 11:28:50 +0200

    Added NodeType enumeration

--- Flask-docker-example/node.py
+++ Flask-docker-example/node.py
@@ -2,12 +2,27 @@
 import copy
 from dataclasses import dataclass
 
+from enum import Enum
+
+
+class NodeType(Enum):
+    """Class to manage all node types."""
+
+    ACTION = "Action"
+    DECISION = "Decision"
+    MERGE = "Merge"
+    INITIAL = "Initial"
+    ACTIVITY_FINAL = "ActivityFinal"
+
+    def __str__(self):
+        return str(self.value)
+
 
 @dataclass
 class Node:
     """Class to define a node."""
 
-    type: str
+    type: NodeType
     name: str
     activity_id: int
     description: str
@@ -19,6 +34,7 @@ class Node:
         node_copy = copy.deepcopy(self.__dict__)
         # node_dict = {}
         node_copy["data"] = {}
+        node_copy["type"] = str(self.type)
         node_copy["data"]["description"] = self.description
         node_copy["data"]["activity_id"] = self.activity_id
         node_copy.pop("description")
@@ -31,7 +47,7 @@ class NodeChange:
     """Class to define a nodechange."""
 
     type: str
-    node_type: str
+    node_type: NodeType
     activity_id: int
     key: str
     node_key: str
@@ -47,7 +63,7 @@ class NodeChange:
         node_change_dict["to"] = node_copy
         node_change_dict["type"] = self.type
         node_change_dict["to"]["position"] = {"x": self.x_axis, "y": self.y_axis}
-        node_change_dict["to"]["type"] = self.node_type
+        node_change_dict["to"]["type"] = str(self.node_type)
         node_change_dict["nodeKey"] = self.node_key
         node_change_dict["key"] = None
         node_change_dict["to"].pop("x_axis")
